a431abe740ed5ccf55f4057099dddfaafbce815c
John Reiner <johnreiner1990@gmail.com>
2019-12-18 19:51:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 def introduction(name)
-  puts "Hi, my name is Josh. \n"
+  puts "Hi, my name is #{name}.\n"
 end
 
 def introduction_with_language(name, language)
